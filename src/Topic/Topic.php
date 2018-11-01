28aa7d3c41cf275562bb03c2b7fb0b47c4cbe55f
daozhu,jing <daozhu.jing@xinxuewen.cn>
2018-11-01 18:55:19 +0800

    add other interface

---
+++ src/Topic/Topic.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * rabbitmq-php
+ *
+ * @copyright daozhu <597180041@qq.com>
+ */
+
+namespace Daozhu\RabbitMQ\Topic;
+
+
+interface Topic
+{
+    /**
+     * topic
+     *
+     * @return mixed
+     */
+    public function topic();
+}
